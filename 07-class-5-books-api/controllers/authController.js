bed847bc39b66b413db20f5e068e294113482fb0
AdrianScharff <adrian18juarez@gmail.com>
2024-06-05 19:09:42 -0600

    Add Login VC

--- 07-class-5-books-api/controllers/authController.js
+++ 07-class-5-books-api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jwt-simple'
 
 const register = async (req, res) => {
   try {
@@ -26,4 +27,43 @@ const register = async (req, res) => {
   }
 }
 
-export { register }
+const login = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({ message: 'email or password are missing' })
+  }
+
+  try {
+    const user = await User.findOne({ email: req.body.email })
+
+    if (!user) {
+      return res.status(401).json({ message: 'email or password incorrect' })
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    )
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'email or password incorrect' })
+    }
+
+    const payload = {
+      email: user.email,
+      role: user.role,
+      iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000 + (7 * 24 * 60 * 60))
+    }
+
+    const token = jwt.encode(payload, process.env.SECRET)
+
+    return res.json({
+      message: 'Login successfully',
+      token
+    })
+  } catch (error) {
+    res.status(500).json({ message: `Login error: ${error}` })
+  }
+}
+
+export { register, login }
