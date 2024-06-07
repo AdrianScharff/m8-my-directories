b653e8cef967fed9e8fdce2620e3a2eeffe25393
AdrianScharff <adrian18juarez@gmail.com>
2024-06-07 09:43:53 -0600

    Add protection to movie routes

--- 08-Cinema-API/routes/movieRoutes.js
+++ 08-Cinema-API/routes/movieRoutes.js
@@ -2,13 +2,14 @@ import express from 'express'
 
 import * as movieController from '../controllers/movieController.js'
 import { isAuth } from '../middlewares/isAuth.js'
+import { isAdmin } from '../middlewares/isAdmin.js'
 
 const movieRoutes = express.Router()
 
-movieRoutes.post('/movies', movieController.createMovie)
-movieRoutes.get('/movies', isAuth, movieController.getAllMovies)
-movieRoutes.get('/movies/:movieId', isAuth, movieController.getOneMovie)
-movieRoutes.patch('/movies/:movieId', movieController.updateMovie)
-movieRoutes.delete('/movies/:movieId', movieController.deleteMovie)
+movieRoutes.post('/movies', isAuth, isAdmin, movieController.createMovie)
+movieRoutes.get('/movies', movieController.getAllMovies)
+movieRoutes.get('/movies/:movieId', movieController.getOneMovie)
+movieRoutes.patch('/movies/:movieId', isAuth, isAdmin, movieController.updateMovie)
+movieRoutes.delete('/movies/:movieId', isAuth, isAdmin, movieController.deleteMovie)
 
 export default movieRoutes
