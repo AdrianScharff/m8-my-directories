ca4a9cf0e0d85084b86d4d76c380bc08dc5b1a1e
AdrianScharff <adrian18juarez@gmail.com>
2024-06-08 10:08:44 -0600

    Fix text in customer routes

--- 08-Cinema-API/routes/users/customerRoutes.js
+++ 08-Cinema-API/routes/users/customerRoutes.js
@@ -6,8 +6,8 @@ import { isAdminOrEmployee } from '../../middlewares/isAdminOrEmployee.js'
 const customerRoutes = express.Router()
 
 customerRoutes.get('/customers', isAuth, isAdminOrEmployee, customerController.getAllCustomers)
-customerRoutes.get('/users/:customerId', isAuth, isAdminOrEmployee, customerController.getOneCustomer)
-customerRoutes.patch('/users/:customerId', isAuth, isAdminOrEmployee, customerController.updateOneCustomer)
-customerRoutes.delete('/users/:customerId', isAuth, isAdminOrEmployee, customerController.deleteOneCustomer)
+customerRoutes.get('/customers/:customerId', isAuth, isAdminOrEmployee, customerController.getOneCustomer)
+customerRoutes.patch('/customers/:customerId', isAuth, isAdminOrEmployee, customerController.updateOneCustomer)
+customerRoutes.delete('/customers/:customerId', isAuth, isAdminOrEmployee, customerController.deleteOneCustomer)
 
 export default customerRoutes
